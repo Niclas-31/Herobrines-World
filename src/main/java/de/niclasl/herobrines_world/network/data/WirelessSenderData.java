c9d15c1896970d479fb373a52c5842f529c380f7
Niclas-31 <niclasloeffler3105@gmail.com>
2026-01-25 14:00:45 +0100

    Update WirelessSenderData.java

--- src/main/java/de/niclasl/herobrines_world/network/data/WirelessSenderData.java
+++ src/main/java/de/niclasl/herobrines_world/network/data/WirelessSenderData.java
@@ -2,12 +2,29 @@ package de.niclasl.herobrines_world.network.data;
 
 import net.minecraft.core.BlockPos;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public record WirelessSenderData(
         BlockPos pos,
         String name,
         String password,
         int range
 ) {
+    private static final Set<BlockPos> receivers = new HashSet<>();
+
+    public void addReceiver(BlockPos receiverPos) {
+        receivers.add(receiverPos);
+    }
+
+    public void removeReceiver(BlockPos receiverPos) {
+        receivers.remove(receiverPos);
+    }
+
+    public Set<BlockPos> getReceivers() {
+        return receivers;
+    }
+
     public boolean hasPassword() {
         return password != null && !password.isEmpty();
     }
