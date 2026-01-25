8651064c438e77f94bf6e77a5a6972f3ba6a5649
Niclas-31 <niclasloeffler3105@gmail.com>
2026-01-25 09:28:19 +0100

    Update WirelessReceiverScreen.java

--- src/main/java/de/niclasl/herobrines_world/client/gui/WirelessReceiverScreen.java
+++ src/main/java/de/niclasl/herobrines_world/client/gui/WirelessReceiverScreen.java
@@ -3,17 +3,24 @@ package de.niclasl.herobrines_world.client.gui;
 import de.niclasl.herobrines_world.client.screens.PasswordPromptScreen;
 import de.niclasl.herobrines_world.network.data.ConnectNetworkPayload;
 import de.niclasl.herobrines_world.network.data.VisibleNetwork;
+import de.niclasl.herobrines_world.network.data.WirelessSenderData;
+import de.niclasl.herobrines_world.network.manager.WirelessNetworkManager;
 import de.niclasl.herobrines_world.world.inventory.custom.WirelessReceiverMenu;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.DyeColor;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 import org.jetbrains.annotations.NotNull;
 
 public class WirelessReceiverScreen
         extends AbstractContainerScreen<WirelessReceiverMenu> {
+
+    private final Font font = Minecraft.getInstance().font;
 
     public WirelessReceiverScreen(WirelessReceiverMenu menu,
                                   Inventory inv, Component title) {
@@ -34,7 +41,10 @@ public class WirelessReceiverScreen
             );
             y += 24;
         }
+
+        System.out.println(x);
     }
+
 
     @Override
     protected void renderBg(@NotNull GuiGraphics guiGraphics, float v, int i, int i1) {
@@ -42,6 +52,16 @@ public class WirelessReceiverScreen
 
     @Override
     protected void renderLabels(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        WirelessSenderData sender = WirelessNetworkManager.getSender(menu.getBlockPos());
+        String senderName = sender != null ? sender.getName() : "None";
+
+        guiGraphics.drawString(
+                this.font,
+                Component.translatable("gui.herobrines_world.connect_with", senderName),
+                135,
+                50,
+                DyeColor.WHITE.getFireworkColor()
+        );
     }
 
     @Override
