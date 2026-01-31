bab75e06de77a7504ba462d7406a7b5e84f21412
Niclas-31 <niclasloeffler3105@gmail.com>
2026-01-31 22:47:32 +0100

    Update SignalColorChangerGuiButton.java

--- src/main/java/de/niclasl/herobrines_world/network/message/SignalColorChangerGuiButton.java
+++ src/main/java/de/niclasl/herobrines_world/network/message/SignalColorChangerGuiButton.java
@@ -45,7 +45,6 @@ public record SignalColorChangerGuiButton(int buttonID, int x, int y, int z) imp
 
 	public static void handleButtonAction(Player entity, int buttonID, int x, int y, int z) {
 		Level world = entity.level();
-		// security measure to prevent arbitrary chunk generation
 		if (!world.hasChunkAt(new BlockPos(x, y, z)))
 			return;
 		if (buttonID == 0) {
