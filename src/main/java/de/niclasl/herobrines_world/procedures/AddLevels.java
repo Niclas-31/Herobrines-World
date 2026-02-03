3b81ed2729e8969a2ad47611847f16d9023de467
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-03 18:22:42 +0100

    Update AddLevels.java

--- src/main/java/de/niclasl/herobrines_world/procedures/AddLevels.java
+++ src/main/java/de/niclasl/herobrines_world/procedures/AddLevels.java
@@ -1,12 +1,12 @@
 package de.niclasl.herobrines_world.procedures;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.commands.CommandSourceStack;
 
 import de.niclasl.herobrines_world.network.ModVariables;
 
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 
 public class AddLevels {
 	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
@@ -14,7 +14,7 @@ public class AddLevels {
 			return;
 		{
 			ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
-			_vars.Soul_Level = entity.getData(ModVariables.PLAYER_VARIABLES).Soul_Level + DoubleArgumentType.getDouble(arguments, "souls");
+			_vars.Soul_Level = entity.getData(ModVariables.PLAYER_VARIABLES).Soul_Level + IntegerArgumentType.getInteger(arguments, "souls");
 			_vars.markSyncDirty();
 		}
 	}
