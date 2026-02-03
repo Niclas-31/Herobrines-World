f1c2628a13f1b54f92ca1805f74e66e97d10ea82
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-03 18:09:22 +0100

    Update SoulReceive.java

--- src/main/java/de/niclasl/herobrines_world/procedures/SoulReceive.java
+++ src/main/java/de/niclasl/herobrines_world/procedures/SoulReceive.java
@@ -4,10 +4,8 @@ import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.registries.Registries;
@@ -16,50 +14,42 @@ import de.niclasl.herobrines_world.network.ModVariables;
 
 @EventBusSubscriber
 public class SoulReceive {
+
+	private static final int SOUL_PER_LEVEL = 100;
+
 	@SubscribeEvent
 	public static void onEntityDeath(LivingDeathEvent event) {
-        execute(event.getEntity().level(), event.getSource().getEntity());
-    }
+		if (!(event.getSource().getEntity() instanceof LivingEntity attacker)) return;
+		if (attacker.level().isClientSide()) return;
 
-	private static void execute(LevelAccessor world, Entity sourceentity) {
-		if (sourceentity == null)
-			return;
-		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
-				.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("herobrines_world:more_souls")))) != 0) {
-			if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
-					.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("herobrines_world:more_souls")))) == 2) {
-				if (sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current <= 100) {
-					{
-						ModVariables.PlayerVariables _vars = sourceentity.getData(ModVariables.PLAYER_VARIABLES);
-						_vars.Soul_Current = sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current + 4;
-						_vars.markSyncDirty();
-					}
-				}
-			} else if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
-					.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("herobrines_world:more_souls")))) == 1) {
-				if (sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current <= 100) {
-					{
-						ModVariables.PlayerVariables _vars = sourceentity.getData(ModVariables.PLAYER_VARIABLES);
-						_vars.Soul_Current = sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current + 2;
-						_vars.markSyncDirty();
-					}
-				}
-			}
+		ModVariables.PlayerVariables vars = attacker.getData(ModVariables.PLAYER_VARIABLES);
+
+		ItemStack stack = attacker.getMainHandItem();
+		int ench = stack.getEnchantmentLevel(
+				attacker.level().registryAccess()
+						.lookupOrThrow(Registries.ENCHANTMENT)
+						.getOrThrow(ResourceKey.create(
+								Registries.ENCHANTMENT,
+								ResourceLocation.parse("herobrines_world:more_souls")
+						))
+		);
+
+		int soulGain = 1;
+
+		if (ench == 1) soulGain = 2;
+		else if (ench == 2) soulGain = 4;
+
+		addSoul(vars, soulGain);
+	}
+
+	private static void addSoul(ModVariables.PlayerVariables vars, int amount) {
+		vars.Soul_Current += amount;
+
+		while (vars.Soul_Current >= SOUL_PER_LEVEL) {
+			vars.Soul_Current -= SOUL_PER_LEVEL;
+			vars.Soul_Level++;
 		}
-		if (sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current <= 100) {
-			{
-				ModVariables.PlayerVariables _vars = sourceentity.getData(ModVariables.PLAYER_VARIABLES);
-				_vars.Soul_Current = sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current + 1;
-				_vars.markSyncDirty();
-			}
-		}
-		if (sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Current > 100) {
-			{
-				ModVariables.PlayerVariables _vars = sourceentity.getData(ModVariables.PLAYER_VARIABLES);
-				_vars.Soul_Level = sourceentity.getData(ModVariables.PLAYER_VARIABLES).Soul_Level + 1;
-				_vars.Soul_Current = 0;
-				_vars.markSyncDirty();
-			}
-		}
+
+		vars.markSyncDirty();
 	}
 }
