8dae839f0da29b02fde174fbe13d85c0ae2ab234
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-03 18:04:17 +0100

    Update SoulAdvertisement.java

--- src/main/java/de/niclasl/herobrines_world/procedures/SoulAdvertisement.java
+++ src/main/java/de/niclasl/herobrines_world/procedures/SoulAdvertisement.java
@@ -4,10 +4,13 @@ import net.minecraft.world.entity.Entity;
 
 import de.niclasl.herobrines_world.network.ModVariables;
 
+import java.text.DecimalFormat;
+
 public class SoulAdvertisement {
 	public static String execute(Entity entity) {
 		if (entity == null)
 			return "";
-		return new java.text.DecimalFormat("##").format(entity.getData(ModVariables.PLAYER_VARIABLES).Soul_Level);
+		return new DecimalFormat("##").format(entity.getData(ModVariables.PLAYER_VARIABLES).Soul_Level);
 	}
+
 }
