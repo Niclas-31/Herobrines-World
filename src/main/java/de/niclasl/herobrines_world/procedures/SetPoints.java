7a5ea6b2abb9868d53e4a51db2e8923506c4a61b
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-03 18:28:25 +0100

    Update SetPoints.java

--- src/main/java/de/niclasl/herobrines_world/procedures/SetPoints.java
+++ src/main/java/de/niclasl/herobrines_world/procedures/SetPoints.java
@@ -1,12 +1,12 @@
 package de.niclasl.herobrines_world.procedures;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.commands.CommandSourceStack;
 
 import de.niclasl.herobrines_world.network.ModVariables;
 
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 
 public class SetPoints {
 	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
@@ -14,8 +14,9 @@ public class SetPoints {
 			return;
 		{
 			ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
-			_vars.Soul_Current = DoubleArgumentType.getDouble(arguments, "souls");
+			_vars.Soul_Current = IntegerArgumentType.getInteger(arguments, "souls");
 			_vars.markSyncDirty();
 		}
 	}
+
 }
