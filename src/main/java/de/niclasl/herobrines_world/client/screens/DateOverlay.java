2302aaa18af495f5d303a85539c5ee567fabf11d
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 19:03:02 +0100

    Update DateOverlay.java

--- src/main/java/de/niclasl/herobrines_world/client/screens/DateOverlay.java
+++ src/main/java/de/niclasl/herobrines_world/client/screens/DateOverlay.java
@@ -9,22 +9,21 @@ import net.neoforged.api.distmarker.Dist;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.client.Minecraft;
 
-import de.niclasl.herobrines_world.procedures.Time;
 import de.niclasl.herobrines_world.procedures.Hide;
-import de.niclasl.herobrines_world.procedures.Date;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class DateOverlay {
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public static void eventHandler(RenderGuiEvent.Pre event) {
 		Player entity = Minecraft.getInstance().player;
+		String date = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
+		String time = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" + Calendar.getInstance().get(Calendar.MINUTE) + ":" + Calendar.getInstance().get(Calendar.SECOND);
         if (Hide.execute(entity)) {
-			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
-
-					Date.execute(), 0, 1, -3407872, false);
-			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
-
-					Time.execute(), 0, 12, -3407872, false);
+			event.getGuiGraphics().drawString(Minecraft.getInstance().font, date, 0, 1, -3407872, false);
+			event.getGuiGraphics().drawString(Minecraft.getInstance().font, time, 0, 12, -3407872, false);
 		}
 	}
 }
