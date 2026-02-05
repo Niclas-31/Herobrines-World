2dbb04abaf9cc64c3937546b17c5f58f4ff1df1e
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 17:53:08 +0100

    Update Souls.java

--- src/main/java/de/niclasl/herobrines_world/command/Souls.java
+++ src/main/java/de/niclasl/herobrines_world/command/Souls.java
@@ -1,77 +1,117 @@
 package de.niclasl.herobrines_world.command;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import de.niclasl.herobrines_world.network.ModVariables;
 import de.niclasl.herobrines_world.procedures.*;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.common.util.FakePlayerFactory;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.Commands;
 
 @EventBusSubscriber
 public class Souls {
+
 	@SubscribeEvent
 	public static void registerCommand(RegisterCommandsEvent event) {
-		event.getDispatcher().register(Commands.literal("souls").requires(s -> s.hasPermission(4))
-				.then(Commands.literal("add").then(Commands.argument("targets", EntityArgument.players()).then(Commands.argument("souls", IntegerArgumentType.integer(0)).then(Commands.literal("levels").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
+		event.getDispatcher().register(
+				Commands.literal("souls")
+						.requires(src -> src.hasPermission(4))
+						.then(Commands.literal("add")
+								.then(Commands.argument("targets", EntityArgument.players())
+										.then(Commands.argument("souls", IntegerArgumentType.integer(0))
+												.then(Commands.literal("levels").executes(ctx -> {
+													int amount = IntegerArgumentType.getInteger(ctx, "souls");
+													for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+														addLevels(player, amount);
+													}
+													return 1;
+												}))
+												.then(Commands.literal("points").executes(ctx -> {
+													int amount = IntegerArgumentType.getInteger(ctx, "souls");
+													for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+														addPoints(player, amount);
+													}
+													return 1;
+												}))
+										)
+								)
+						)
+						.then(Commands.literal("set")
+								.then(Commands.argument("targets", EntityArgument.players())
+										.then(Commands.argument("souls", IntegerArgumentType.integer(0))
+												.then(Commands.literal("levels").executes(ctx -> {
+													int amount = IntegerArgumentType.getInteger(ctx, "souls");
+													for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+														setLevels(player, amount);
+													}
+													return 1;
+												}))
+												.then(Commands.literal("points").executes(ctx -> {
+													int amount = IntegerArgumentType.getInteger(ctx, "souls");
+													for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+														setPoints(player, amount);
+													}
+													return 1;
+												}))
+										)
+								)
+						)
+						.then(Commands.literal("query")
+								.then(Commands.argument("targets", EntityArgument.players())
+										.then(Commands.literal("levels").executes(ctx -> {
+											for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+												int level = player.getData(ModVariables.PLAYER_VARIABLES).Soul_Level;
+												ctx.getSource().sendSuccess(
+														() -> Component.literal(player.getName().getString() + " Levels: " + level),
+														false
+												);
+											}
+											return 1;
+										}))
+										.then(Commands.literal("points").executes(ctx -> {
+											for (ServerPlayer player : EntityArgument.getPlayers(ctx, "targets")) {
+												int points = player.getData(ModVariables.PLAYER_VARIABLES).Soul_Current;
+												ctx.getSource().sendSuccess(
+														() -> Component.literal(player.getName().getString() + " Points: " + points),
+														false
+												);
+											}
+											return 1;
+										}))
+								)
+						)
+		);
+	}
+	
+	private static void addLevels(ServerPlayer player, int amount) {
+		ModVariables.PlayerVariables vars = player.getData(ModVariables.PLAYER_VARIABLES);
+		vars.Soul_Level += amount;
+		vars.markSyncDirty();
+	}
 
-					AddLevels.execute(arguments, entity);
-					return 0;
-				})).then(Commands.literal("points").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
+	private static void addPoints(ServerPlayer player, int amount) {
+		ModVariables.PlayerVariables vars = player.getData(ModVariables.PLAYER_VARIABLES);
+		vars.Soul_Current += amount;
+		while (vars.Soul_Current >= 100) {
+			vars.Soul_Current -= 100;
+			vars.Soul_Level += 1;
+		}
+		vars.markSyncDirty();
+	}
 
-					AddPoints.execute(arguments, entity);
-					return 0;
-				}))))).then(Commands.literal("set").then(Commands.argument("target", EntityArgument.players()).then(Commands.argument("souls", IntegerArgumentType.integer(0)).then(Commands.literal("levels").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
+	private static void setLevels(ServerPlayer player, int amount) {
+		ModVariables.PlayerVariables vars = player.getData(ModVariables.PLAYER_VARIABLES);
+		vars.Soul_Level = amount;
+		vars.markSyncDirty();
+	}
 
-					SetLevels.execute(arguments, entity);
-					return 0;
-				})).then(Commands.literal("points").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
-
-					SetPoints.execute(arguments, entity);
-					return 0;
-				}))))).then(Commands.literal("query").then(Commands.argument("targets", EntityArgument.player()).then(Commands.literal("levels").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
-
-					QueryLevels.execute(entity);
-					return 0;
-				})).then(Commands.literal("points").executes(arguments -> {
-					Level world = arguments.getSource().getUnsidedLevel();
-					Entity entity = arguments.getSource().getEntity();
-					if (entity == null && world instanceof ServerLevel _servLevel)
-						entity = FakePlayerFactory.getMinecraft(_servLevel);
-					if (entity != null) entity.getDirection();
-
-					QueryPoints.execute(entity);
-					return 0;
-				})))));
+	private static void setPoints(ServerPlayer player, int amount) {
+		ModVariables.PlayerVariables vars = player.getData(ModVariables.PLAYER_VARIABLES);
+		vars.Soul_Current = amount;
+		vars.markSyncDirty();
 	}
 }
