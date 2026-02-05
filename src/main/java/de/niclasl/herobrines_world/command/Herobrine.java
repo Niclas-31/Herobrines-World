47f2b7eb359a37adb361fedb953cabd864d5595a
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 17:08:43 +0100

    Update Herobrine.java

--- src/main/java/de/niclasl/herobrines_world/command/Herobrine.java
+++ src/main/java/de/niclasl/herobrines_world/command/Herobrine.java
@@ -1,175 +1,90 @@
 package de.niclasl.herobrines_world.command;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.GameType;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.common.util.FakePlayerFactory;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.Commands;
 
-import de.niclasl.herobrines_world.procedures.HerobrineInt;
-import de.niclasl.herobrines_world.procedures.Gamemode3;
-import de.niclasl.herobrines_world.procedures.Gamemode2;
-import de.niclasl.herobrines_world.procedures.Gamemode1;
-import de.niclasl.herobrines_world.procedures.Gamemode0;
-
 import com.mojang.brigadier.arguments.DoubleArgumentType;
+
+import java.util.Collection;
+import java.util.List;
 
 @EventBusSubscriber
 public class Herobrine {
 	@SubscribeEvent
 	public static void registerCommand(RegisterCommandsEvent event) {
-		event.getDispatcher().register(Commands.literal("herobrine").requires(s -> s.hasPermission(4)).then(Commands.literal("survival").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-            if (entity != null) entity.getDirection();
 
-			Gamemode0.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-            if (entity != null) entity.getDirection();
-
-			Gamemode0.execute(entity);
-			return 0;
-		}))).then(Commands.literal("creative").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-            if (entity != null) entity.getDirection();
-
-			Gamemode1.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-            if (entity != null) entity.getDirection();
-
-			Gamemode1.execute(entity);
-			return 0;
-		}))).then(Commands.literal("adventure").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-            if (entity != null) entity.getDirection();
-
-			Gamemode2.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode2.execute(entity);
-			return 0;
-		}))).then(Commands.literal("spectator").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode3.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode3.execute(entity);
-			return 0;
-		}))).then(Commands.literal("s").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode0.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode0.execute(entity);
-			return 0;
-		}))).then(Commands.literal("c").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode1.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode1.execute(entity);
-			return 0;
-		}))).then(Commands.literal("a").executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			Gamemode2.execute(entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-
-			if (entity != null) entity.getDirection();
-
-			Gamemode2.execute(entity);
-			return 0;
-		}))).then(Commands.argument("int", DoubleArgumentType.doubleArg(0, 3)).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-
-			if (entity != null) entity.getDirection();
-
-			HerobrineInt.execute(arguments, entity);
-			return 0;
-		}).then(Commands.argument("target", EntityArgument.players()).executes(arguments -> {
-			Level world = arguments.getSource().getUnsidedLevel();
-			Entity entity = arguments.getSource().getEntity();
-			if (entity == null && world instanceof ServerLevel _servLevel)
-				entity = FakePlayerFactory.getMinecraft(_servLevel);
-			if (entity != null) entity.getDirection();
-
-			HerobrineInt.execute(arguments, entity);
-			return 0;
-		}))));
+		event.getDispatcher().register(
+				Commands.literal("herobrine")
+						.requires(s -> s.hasPermission(4))
+						
+						.then(modeCommand("survival", GameType.SURVIVAL))
+						.then(modeCommand("s", GameType.SURVIVAL))
+						
+						.then(modeCommand("creative", GameType.CREATIVE))
+						.then(modeCommand("c", GameType.CREATIVE))
+						
+						.then(modeCommand("adventure", GameType.ADVENTURE))
+						.then(modeCommand("a", GameType.ADVENTURE))
+						
+						.then(modeCommand("spectator", GameType.SPECTATOR))
+						
+						.then(
+								Commands.argument("mode", DoubleArgumentType.doubleArg(0,3))
+										.executes(ctx -> {
+											setGamemode(
+													List.of(ctx.getSource().getPlayerOrException()),
+													(int) DoubleArgumentType.getDouble(ctx, "mode"));
+											return 1;
+										})
+										.then(Commands.argument("target", EntityArgument.players())
+												.executes(ctx -> {
+													setGamemode(
+															EntityArgument.getPlayers(ctx, "target"),
+															(int) DoubleArgumentType.getDouble(ctx, "mode"));
+													return 1;
+												})
+										)
+						)
+		);
 	}
 
+	private static LiteralArgumentBuilder<CommandSourceStack> modeCommand(String name, GameType type) {
+		return Commands.literal(name)
+				.executes(ctx -> {
+					setGamemode(
+							List.of(ctx.getSource().getPlayerOrException()),
+							type.getId());
+					return 1;
+				})
+				.then(Commands.argument("target", EntityArgument.players())
+						.executes(ctx -> {
+							setGamemode(
+									EntityArgument.getPlayers(ctx, "target"),
+									type.getId());
+							return 1;
+						})
+				);
+	}
+
+	private static void setGamemode(Collection<ServerPlayer> players, int mode) {
+
+		GameType gameType = switch (mode) {
+            case 1 -> GameType.CREATIVE;
+			case 2 -> GameType.ADVENTURE;
+			case 3 -> GameType.SPECTATOR;
+			default -> GameType.SURVIVAL;
+		};
+
+		for (ServerPlayer player : players) {
+			player.setGameMode(gameType);
+		}
+	}
 }
