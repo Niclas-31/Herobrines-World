3f2265f09d8ea4460670baa2dad4264f4d51b5fe
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 19:23:27 +0100

    Delete src/main/java/de/niclasl/herobrines_world/procedures/Time.java

--- src/main/java/de/niclasl/herobrines_world/procedures/Time.java
+++
@@ -1,9 +0,0 @@
-package de.niclasl.herobrines_world.procedures;
-
-import java.util.Calendar;
-
-public class Time {
-	public static String execute() {
-		return Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" + Calendar.getInstance().get(Calendar.MINUTE) + ":" + Calendar.getInstance().get(Calendar.SECOND);
-	}
-}
