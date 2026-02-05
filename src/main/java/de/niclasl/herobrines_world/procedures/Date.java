469a444a6a9f83ca44201d31ccf0ae5617e363ce
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 19:12:27 +0100

    Delete src/main/java/de/niclasl/herobrines_world/procedures/Date.java

--- src/main/java/de/niclasl/herobrines_world/procedures/Date.java
+++
@@ -1,9 +0,0 @@
-package de.niclasl.herobrines_world.procedures;
-
-import java.util.Calendar;
-
-public class Date {
-	public static String execute() {
-		return new java.text.SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
-	}
-}
