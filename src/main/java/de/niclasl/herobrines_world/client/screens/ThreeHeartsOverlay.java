1987d8c8203c1f8795845fb890b9eb5222cea318
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 19:26:16 +0100

    Update ThreeHeartsOverlay.java

--- src/main/java/de/niclasl/herobrines_world/client/screens/ThreeHeartsOverlay.java
+++ src/main/java/de/niclasl/herobrines_world/client/screens/ThreeHeartsOverlay.java
@@ -1,5 +1,13 @@
 package de.niclasl.herobrines_world.client.screens;
 
+import de.niclasl.herobrines_world.init.ModGameRules;
+import de.niclasl.herobrines_world.network.ModVariables;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.LevelAccessor;
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,14 +19,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.Minecraft;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
 
-import de.niclasl.herobrines_world.procedures.ThreeHeartsOverlayDisplay;
-import de.niclasl.herobrines_world.procedures.Heart3;
-import de.niclasl.herobrines_world.procedures.Heart2;
-import de.niclasl.herobrines_world.procedures.Heart1;
+import java.util.Objects;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class ThreeHeartsOverlay {
+
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public static void eventHandler(RenderGuiEvent.Pre event) {
 		int w = event.getGuiGraphics().guiWidth();
@@ -28,22 +36,81 @@ public class ThreeHeartsOverlay {
 		if (entity != null) {
 			world = entity.level();
 		}
-		if (ThreeHeartsOverlayDisplay.execute(world, entity)) {
+
+		boolean gamemode = getEntityGameType(entity) == GameType.SURVIVAL || getEntityGameType(entity) == GameType.ADVENTURE;
+        assert entity != null;
+        boolean heart1 = entity.getData(ModVariables.PLAYER_VARIABLES).Hearts >= 1;
+		boolean heart2 = entity.getData(ModVariables.PLAYER_VARIABLES).Hearts >= 2;
+		boolean heart3 = entity.getData(ModVariables.PLAYER_VARIABLES).Hearts == 3;
+
+		if (ModVariables.MapVariables.get(world).ThreeHearts && gamemode) {
 			event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/container.png"), w / 2 - 10, h - 47, 0, 0, 9, 9, 9, 9);
 
 			event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/container.png"), w / 2, h - 47, 0, 0, 9, 9, 9, 9);
 
 			event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/container.png"), w / 2 - 5, h - 55, 0, 0, 9, 9, 9, 9);
 
-			if (Heart3.execute(entity)) {
+			if (heart3) {
 				event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/frozen_full.png"), w / 2 - 5, h - 55, 0, 0, 9, 9, 9, 9);
 			}
-			if (Heart1.execute(entity)) {
+			if (heart1) {
 				event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/frozen_full.png"), w / 2, h - 47, 0, 0, 9, 9, 9, 9);
 			}
-			if (Heart2.execute(entity)) {
+			if (heart2) {
 				event.getGuiGraphics().blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.parse("herobrines_world:textures/screens/frozen_full.png"), w / 2 - 10, h - 47, 0, 0, 9, 9, 9, 9);
 			}
 		}
+	}
+
+	private static GameType getEntityGameType(Entity entity) {
+		if (entity instanceof ServerPlayer serverPlayer) {
+			return serverPlayer.gameMode.getGameModeForPlayer();
+		} else if (entity instanceof Player player && player.level().isClientSide()) {
+			PlayerInfo playerInfo = Objects.requireNonNull(Minecraft.getInstance().getConnection()).getPlayerInfo(player.getGameProfile().id());
+			if (playerInfo != null)
+				return playerInfo.getGameMode();
+		}
+		return null;
+	}
+
+	@SubscribeEvent
+	public static void onEntityDeath(LivingDeathEvent event) {
+		execute(event.getEntity().level(), event.getEntity());
+	}
+
+	private static void execute(LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		if (ModVariables.MapVariables.get(world).ThreeHearts) {
+			if (entity.getData(ModVariables.PLAYER_VARIABLES).Hearts > 0) {
+				{
+					ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
+					_vars.Hearts = entity.getData(ModVariables.PLAYER_VARIABLES).Hearts - 1;
+					_vars.markSyncDirty();
+				}
+			}
+			if (entity.getData(ModVariables.PLAYER_VARIABLES).Hearts == 0) {
+				if (entity instanceof ServerPlayer _player)
+					_player.setGameMode(GameType.SPECTATOR);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load event) {
+		if (!(event.getLevel() instanceof ServerLevel level)) return;
+
+		if (level.getLevelData().isHardcore()) {
+			level.getGameRules()
+					.getRule(ModGameRules.THREE_HEARTS)
+					.set(false, level.getServer());
+
+			ModVariables.MapVariables.get(level).ThreeHearts = false;
+			ModVariables.MapVariables.get(level).markSyncDirty();
+			return;
+		}
+
+		ModVariables.MapVariables.get(level).ThreeHearts = level.getGameRules().getBoolean(ModGameRules.THREE_HEARTS);
+		ModVariables.MapVariables.get(level).markSyncDirty();
 	}
 }
