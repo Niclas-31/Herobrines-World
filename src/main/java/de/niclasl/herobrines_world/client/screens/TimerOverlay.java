4bfafad9b34e7d298647d921fee9721590518729
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-05 19:37:31 +0100

    Update TimerOverlay.java

--- src/main/java/de/niclasl/herobrines_world/client/screens/TimerOverlay.java
+++ src/main/java/de/niclasl/herobrines_world/client/screens/TimerOverlay.java
@@ -1,5 +1,10 @@
 package de.niclasl.herobrines_world.client.screens;
 
+import de.niclasl.herobrines_world.network.ModVariables;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.GameType;
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -9,8 +14,8 @@ import net.neoforged.api.distmarker.Dist;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.client.Minecraft;
 
-import de.niclasl.herobrines_world.procedures.TimerText;
-import de.niclasl.herobrines_world.procedures.TimerOverlayInGame;
+import java.text.DecimalFormat;
+import java.util.Objects;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class TimerOverlay {
@@ -19,10 +24,25 @@ public class TimerOverlay {
 		int w = event.getGuiGraphics().guiWidth();
 		int h = event.getGuiGraphics().guiHeight();
 		Player entity = Minecraft.getInstance().player;
-		if (TimerOverlayInGame.execute(entity)) {
-			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
+        assert entity != null;
+        String timer = new DecimalFormat("####").format(entity.getData(ModVariables.PLAYER_VARIABLES).Day) + ":" + new DecimalFormat("##").format(entity.getData(ModVariables.PLAYER_VARIABLES).Hour) + ":"
+				+ new DecimalFormat("##").format(entity.getData(ModVariables.PLAYER_VARIABLES).Minute) + ":" + new DecimalFormat("##").format(entity.getData(ModVariables.PLAYER_VARIABLES).Second);
 
-					TimerText.execute(entity), w / 2 - 19, h - 63, -65536, false);
+		boolean condition = entity.getData(ModVariables.PLAYER_VARIABLES).TimerActive && (getEntityGameType(entity) == GameType.SURVIVAL || getEntityGameType(entity) == GameType.ADVENTURE);
+
+		if (condition) {
+			event.getGuiGraphics().drawString(Minecraft.getInstance().font, timer, w / 2 - 19, h - 63, -65536, false);
 		}
+	}
+
+	private static GameType getEntityGameType(Entity entity) {
+		if (entity instanceof ServerPlayer serverPlayer) {
+			return serverPlayer.gameMode.getGameModeForPlayer();
+		} else if (entity instanceof Player player && player.level().isClientSide()) {
+			PlayerInfo playerInfo = Objects.requireNonNull(Minecraft.getInstance().getConnection()).getPlayerInfo(player.getGameProfile().id());
+			if (playerInfo != null)
+				return playerInfo.getGameMode();
+		}
+		return null;
 	}
 }
