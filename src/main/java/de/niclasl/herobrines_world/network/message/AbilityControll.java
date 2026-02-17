c520ee517730fe905102f5cd2e3daf47a3a7ea53
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-17 15:43:10 +0100

    Update AbilityControll.java

--- src/main/java/de/niclasl/herobrines_world/network/message/AbilityControll.java
+++ src/main/java/de/niclasl/herobrines_world/network/message/AbilityControll.java
@@ -1,5 +1,7 @@
 package de.niclasl.herobrines_world.network.message;
 
+import de.niclasl.herobrines_world.network.ModVariables;
+import de.niclasl.herobrines_world.procedures.HerobrineArmorAbility;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,41 +16,53 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 
-import de.niclasl.herobrines_world.procedures.HerobrineArmorAbilityControll;
 import de.niclasl.herobrines_world.HerobrinesWorld;
 import org.jetbrains.annotations.NotNull;
 
 @EventBusSubscriber
 public record AbilityControll(int eventType) implements CustomPacketPayload {
-	public static final Type<AbilityControll> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(HerobrinesWorld.MODID, "key_ability_controll"));
-	public static final StreamCodec<RegistryFriendlyByteBuf, AbilityControll> STREAM_CODEC = StreamCodec.of((RegistryFriendlyByteBuf buffer, AbilityControll message) -> buffer.writeInt(message.eventType), (RegistryFriendlyByteBuf buffer) -> new AbilityControll(buffer.readInt()));
+    public static final Type<AbilityControll> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(HerobrinesWorld.MODID, "key_ability_controll"));
+    public static final StreamCodec<RegistryFriendlyByteBuf, AbilityControll> STREAM_CODEC = StreamCodec.of((RegistryFriendlyByteBuf buffer, AbilityControll message) -> buffer.writeInt(message.eventType), (RegistryFriendlyByteBuf buffer) -> new AbilityControll(buffer.readInt()));
 
-	@Override
-	public @NotNull Type<AbilityControll> type() {
-		return TYPE;
-	}
+    @Override
+    public @NotNull Type<AbilityControll> type() {
+        return TYPE;
+    }
 
-	public static void handleData(final AbilityControll message, final IPayloadContext context) {
-		if (context.flow() == PacketFlow.SERVERBOUND) {
-			context.enqueueWork(() -> pressAction(context.player(), message.eventType)).exceptionally(e -> {
-				context.connection().disconnect(Component.literal(e.getMessage()));
-				return null;
-			});
-		}
-	}
+    public static void handleData(final AbilityControll message, final IPayloadContext context) {
+        if (context.flow() == PacketFlow.SERVERBOUND) {
+            context.enqueueWork(() -> pressAction(context.player(), message.eventType)).exceptionally(e -> {
+                context.connection().disconnect(Component.literal(e.getMessage()));
+                return null;
+            });
+        }
+    }
 
-	public static void pressAction(Player entity, int type) {
-		Level world = entity.level();
-		if (!world.hasChunkAt(entity.blockPosition()))
-			return;
-		if (type == 0) {
+    public static void pressAction(Player entity, int type) {
+        Level world = entity.level();
+        if (!world.hasChunkAt(entity.blockPosition()))
+            return;
+        if (type == 0) {
+            if (entity.getData(ModVariables.PLAYER_VARIABLES).AbilityActive) {
+                {
+                    ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
+                    _vars.AbilityActive = false;
+                    _vars.markSyncDirty();
+                }
+                HerobrineArmorAbility.execute(entity);
+            } else {
+                {
+                    ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
+                    _vars.AbilityActive = true;
+                    _vars.markSyncDirty();
+                }
+                HerobrineArmorAbility.execute(entity);
+            }
+        }
+    }
 
-			HerobrineArmorAbilityControll.execute(entity);
-		}
-	}
-
-	@SubscribeEvent
-	public static void registerMessage(FMLCommonSetupEvent event) {
-		HerobrinesWorld.addNetworkMessage(AbilityControll.TYPE, AbilityControll.STREAM_CODEC, AbilityControll::handleData);
-	}
+    @SubscribeEvent
+    public static void registerMessage(FMLCommonSetupEvent event) {
+        HerobrinesWorld.addNetworkMessage(AbilityControll.TYPE, AbilityControll.STREAM_CODEC, AbilityControll::handleData);
+    }
 }
