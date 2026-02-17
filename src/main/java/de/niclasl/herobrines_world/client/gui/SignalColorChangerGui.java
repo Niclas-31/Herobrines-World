f431a92feed60ab9a8ac0882987ac277b5d2789b
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-17 11:29:00 +0100

    Update SignalColorChangerGui.java

--- src/main/java/de/niclasl/herobrines_world/client/gui/SignalColorChangerGui.java
+++ src/main/java/de/niclasl/herobrines_world/client/gui/SignalColorChangerGui.java
@@ -92,8 +92,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		super.init();
         ImageButton imagebutton_green_gui_button = new ImageButton(this.leftPos - 21, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/green_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/green_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(0, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 0, x, y, z);
         }) {
@@ -105,8 +103,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_green_gui_button);
         ImageButton imagebutton_yellow_gui_button = new ImageButton(this.leftPos - 63, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/yellow_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/yellow_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(1, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 1, x, y, z);
         }) {
@@ -118,8 +114,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_yellow_gui_button);
         ImageButton imagebutton_red_gui_button = new ImageButton(this.leftPos - 104, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/red_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/red_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(2, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 2, x, y, z);
         }) {
@@ -131,8 +125,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_red_gui_button);
         ImageButton imagebutton_blue_gui_button = new ImageButton(this.leftPos + 20, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/blue_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/blue_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(3, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 3, x, y, z);
         }) {
@@ -144,8 +136,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_blue_gui_button);
         ImageButton imagebutton_cyan_gui_button = new ImageButton(this.leftPos + 63, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/cyan_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/cyan_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(4, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 4, x, y, z);
         }) {
@@ -157,8 +147,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_cyan_gui_button);
         ImageButton imagebutton_gray_gui_button = new ImageButton(this.leftPos - 104, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/gray_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/gray_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(5, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 5, x, y, z);
         }) {
@@ -170,8 +158,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_gray_gui_button);
         ImageButton imagebutton_light_blue_gui_button = new ImageButton(this.leftPos - 63, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/light_blue_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/light_blue_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(6, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 6, x, y, z);
         }) {
@@ -183,8 +169,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_light_blue_gui_button);
         ImageButton imagebutton_light_gray_gui_button = new ImageButton(this.leftPos - 21, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/light_gray_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/light_gray_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(7, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 7, x, y, z);
         }) {
@@ -196,8 +180,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_light_gray_gui_button);
         ImageButton imagebutton_lime_gui_button = new ImageButton(this.leftPos + 20, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/lime_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/lime_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(8, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 8, x, y, z);
         }) {
@@ -209,8 +191,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_lime_gui_button);
         ImageButton imagebutton_magenta_gui_button = new ImageButton(this.leftPos + 63, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/magenta_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/magenta_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(9, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 9, x, y, z);
         }) {
@@ -222,8 +202,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_magenta_gui_button);
         ImageButton imagebutton_orange_gui_button = new ImageButton(this.leftPos + 106, this.topPos - 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/orange_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/orange_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(10, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 10, x, y, z);
         }) {
@@ -235,8 +213,6 @@ public class SignalColorChangerGui extends AbstractContainerScreen<SignalColorCh
 		this.addRenderableWidget(imagebutton_orange_gui_button);
         ImageButton imagebutton_pink_gui_button = new ImageButton(this.leftPos + 106, this.topPos + 20, 24, 24,
                 new WidgetSprites(ResourceLocation.parse("herobrines_world:textures/screens/pink_gui_button.png"), ResourceLocation.parse("herobrines_world:textures/screens/pink_gui_button.png")), e -> {
-            int x = SignalColorChangerGui.this.x;
-            int y = SignalColorChangerGui.this.y;
             ClientPacketDistributor.sendToServer(new SignalColorChangerGuiButton(11, x, y, z));
             SignalColorChangerGuiButton.handleButtonAction(entity, 11, x, y, z);
         }) {
