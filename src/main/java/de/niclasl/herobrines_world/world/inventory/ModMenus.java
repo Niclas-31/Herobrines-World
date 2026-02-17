dcfca152f4b36aefdd01423118239c210e7ac73c
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-17 14:47:00 +0100

    Update ModMenus.java

--- src/main/java/de/niclasl/herobrines_world/world/inventory/ModMenus.java
+++ src/main/java/de/niclasl/herobrines_world/world/inventory/ModMenus.java
@@ -1,24 +1,16 @@
 package de.niclasl.herobrines_world.world.inventory;
 
-import de.niclasl.herobrines_world.client.ModScreens;
 import de.niclasl.herobrines_world.world.inventory.custom.*;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
-import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.client.Minecraft;
 
-import de.niclasl.herobrines_world.network.message.MenuStateUpdate;
 import de.niclasl.herobrines_world.HerobrinesWorld;
 
-import java.awt.*;
 import java.util.Map;
 
 public class ModMenus {
@@ -35,25 +27,5 @@ public class ModMenus {
 
 	public interface MenuAccessor {
 		Map<String, Object> getMenuState();
-
-		default void sendMenuStateUpdate(Player player, int elementType, String name, Object elementState, boolean needClientUpdate) {
-			getMenuState().put(elementType + ":" + name, elementState);
-			if (player instanceof ServerPlayer serverPlayer) {
-				PacketDistributor.sendToPlayer(serverPlayer, new MenuStateUpdate(elementType, name, elementState));
-			} else if (player.level().isClientSide()) {
-				if (Minecraft.getInstance().screen instanceof ModScreens.ScreenAccessor accessor && needClientUpdate)
-					accessor.updateMenuState(elementType, name, elementState);
-				ClientPacketDistributor.sendToServer(new MenuStateUpdate(elementType, name, elementState));
-			}
-		}
-
-		@SuppressWarnings("unchecked")
-		default <T> T getMenuState(int elementType, String name, T defaultValue) {
-			try {
-				return (T) getMenuState().getOrDefault(elementType + ":" + name, defaultValue);
-			} catch (ClassCastException e) {
-				return defaultValue;
-			}
-		}
 	}
 }
