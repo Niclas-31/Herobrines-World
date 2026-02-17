816592bb13f56bf3d9a4cb1e3f84f80f4e5235c9
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-17 15:39:01 +0100

    Update ControllVariableTimer.java

--- src/main/java/de/niclasl/herobrines_world/network/message/ControllVariableTimer.java
+++ src/main/java/de/niclasl/herobrines_world/network/message/ControllVariableTimer.java
@@ -1,5 +1,6 @@
 package de.niclasl.herobrines_world.network.message;
 
+import de.niclasl.herobrines_world.network.ModVariables;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,41 +15,55 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 
-import de.niclasl.herobrines_world.procedures.ControllVariable;
 import de.niclasl.herobrines_world.HerobrinesWorld;
 import org.jetbrains.annotations.NotNull;
 
 @EventBusSubscriber
 public record ControllVariableTimer(int eventType) implements CustomPacketPayload {
-	public static final Type<ControllVariableTimer> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(HerobrinesWorld.MODID, "key_controll_variable_timer"));
-	public static final StreamCodec<RegistryFriendlyByteBuf, ControllVariableTimer> STREAM_CODEC = StreamCodec.of((RegistryFriendlyByteBuf buffer, ControllVariableTimer message) -> buffer.writeInt(message.eventType), (RegistryFriendlyByteBuf buffer) -> new ControllVariableTimer(buffer.readInt()));
+    public static final Type<ControllVariableTimer> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(HerobrinesWorld.MODID, "key_controll_variable_timer"));
+    public static final StreamCodec<RegistryFriendlyByteBuf, ControllVariableTimer> STREAM_CODEC = StreamCodec.of((RegistryFriendlyByteBuf buffer, ControllVariableTimer message) -> buffer.writeInt(message.eventType), (RegistryFriendlyByteBuf buffer) -> new ControllVariableTimer(buffer.readInt()));
 
-	@Override
-	public @NotNull Type<ControllVariableTimer> type() {
-		return TYPE;
-	}
+    @Override
+    public @NotNull Type<ControllVariableTimer> type() {
+        return TYPE;
+    }
 
-	public static void handleData(final ControllVariableTimer message, final IPayloadContext context) {
-		if (context.flow() == PacketFlow.SERVERBOUND) {
-			context.enqueueWork(() -> pressAction(context.player(), message.eventType)).exceptionally(e -> {
-				context.connection().disconnect(Component.literal(e.getMessage()));
-				return null;
-			});
-		}
-	}
+    public static void handleData(final ControllVariableTimer message, final IPayloadContext context) {
+        if (context.flow() == PacketFlow.SERVERBOUND) {
+            context.enqueueWork(() -> pressAction(context.player(), message.eventType)).exceptionally(e -> {
+                context.connection().disconnect(Component.literal(e.getMessage()));
+                return null;
+            });
+        }
+    }
 
-	public static void pressAction(Player entity, int type) {
-		Level world = entity.level();
-		if (!world.hasChunkAt(entity.blockPosition()))
-			return;
-		if (type == 0) {
+    public static void pressAction(Player entity, int type) {
+        Level world = entity.level();
+        if (!world.hasChunkAt(entity.blockPosition()))
+            return;
+        if (type == 0) {
+            if (entity.getData(ModVariables.PLAYER_VARIABLES).TimerActive) {
+                {
+                    ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
+                    _vars.TimerActive = false;
+                    _vars.markSyncDirty();
+                }
+                if (entity instanceof Player _player && !_player.level().isClientSide())
+                    _player.displayClientMessage(Component.literal("Timer stopped"), true);
+            } else {
+                {
+                    ModVariables.PlayerVariables _vars = entity.getData(ModVariables.PLAYER_VARIABLES);
+                    _vars.TimerActive = true;
+                    _vars.markSyncDirty();
+                }
+                if (entity instanceof Player _player && !_player.level().isClientSide())
+                    _player.displayClientMessage(Component.literal("Timer continued"), true);
+            }
+        }
+    }
 
-			ControllVariable.execute(entity);
-		}
-	}
-
-	@SubscribeEvent
-	public static void registerMessage(FMLCommonSetupEvent event) {
-		HerobrinesWorld.addNetworkMessage(ControllVariableTimer.TYPE, ControllVariableTimer.STREAM_CODEC, ControllVariableTimer::handleData);
-	}
+    @SubscribeEvent
+    public static void registerMessage(FMLCommonSetupEvent event) {
+        HerobrinesWorld.addNetworkMessage(ControllVariableTimer.TYPE, ControllVariableTimer.STREAM_CODEC, ControllVariableTimer::handleData);
+    }
 }
