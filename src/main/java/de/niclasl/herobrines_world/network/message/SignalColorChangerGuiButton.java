f10417ec510e990a7cd9dbec6b5ca024171e822c
Niclas-31 <niclasloeffler3105@gmail.com>
2026-02-17 11:28:12 +0100

    Update SignalColorChangerGuiButton.java

--- src/main/java/de/niclasl/herobrines_world/network/message/SignalColorChangerGuiButton.java
+++ src/main/java/de/niclasl/herobrines_world/network/message/SignalColorChangerGuiButton.java
@@ -1,6 +1,10 @@
 package de.niclasl.herobrines_world.network.message;
 
 import de.niclasl.herobrines_world.procedures.*;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -48,53 +52,172 @@ public record SignalColorChangerGuiButton(int buttonID, int x, int y, int z) imp
 		if (!world.hasChunkAt(new BlockPos(x, y, z)))
 			return;
 		if (buttonID == 0) {
+			String value = "green";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnGreenButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 1) {
+			String value = "yellow";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnYellowButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 2) {
+			String value = "red";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnRedButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 3) {
+			String value = "blue";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnBlueButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 4) {
+			String value = "cyan";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnCyanButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 5) {
+			String value = "gray";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnGrayButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 6) {
+			String value = "light_blue";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnLightBlueButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 7) {
+			String value = "light_gray";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnLightGrayButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 8) {
+			String value = "lime";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnLimeButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 9) {
+			String value = "magenta";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnMagentaButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 10) {
+			String value = "orange";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnOrangeButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
 		if (buttonID == 11) {
+			String value = "pink";
+			BlockPos pos = BlockPos.containing(x, y, z);
+			BlockState state = world.getBlockState(pos);
 
-			OnPinkButtonClicked.execute(world, x, y, z);
+			Property<?> property = state.getBlock()
+					.getStateDefinition()
+					.getProperty("color");
+
+			if (property instanceof EnumProperty<?> enumProp) {
+				setEnumValue(world, pos, state, enumProp, value);
+			}
 		}
+	}
+
+	private static <T extends Enum<T> & StringRepresentable> void setEnumValue(
+			Level world,
+			BlockPos pos,
+			BlockState state,
+			EnumProperty<T> property,
+			String value
+	) {
+		property.getValue(value).ifPresent(val ->
+				world.setBlock(pos, state.setValue(property, val), 3));
 	}
 
 	@SubscribeEvent
