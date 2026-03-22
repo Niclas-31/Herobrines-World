5ca4583549a3ac36591549be792c260806da3fcc
Niclas-31 <niclasloeffler3105@gmail.com>
2026-03-22 09:09:03 +0100

    Save all local changes

---
+++ src/main/java/de/niclasl/herobrines_world/block/custom/AutoFarmerBlock.java
@@ -0,0 +1,25 @@
+package de.niclasl.herobrines_world.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class AutoFarmerBlock extends BaseEntityBlock {
+
+    public AutoFarmerBlock(Properties p_49224_) {
+        super(p_49224_);
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return null;
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }
+}
