cab3e8ed522ee8209b75e79614a95c63e088cafb
Niclas-31 <niclasloeffler3105@gmail.com>
2026-03-24 17:48:00 +0100

    Initial commit

--- src/main/java/de/niclasl/herobrines_world/datagen/ModDatapackProvider.java
+++ src/main/java/de/niclasl/herobrines_world/datagen/ModDatapackProvider.java
@@ -2,8 +2,6 @@ package de.niclasl.herobrines_world.datagen;
 
 import de.niclasl.herobrines_world.HerobrinesWorld;
 import de.niclasl.herobrines_world.enchantment.ModEnchantments;
-import de.niclasl.herobrines_world.trim.ModTrimMaterials;
-import de.niclasl.herobrines_world.trim.ModTrimPatterns;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -15,8 +13,6 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
-            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
             .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
